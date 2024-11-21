6120abc948f9e87be76874be09cc733d6029df88
ElectroHeavenVN <98677996+ElectroHeavenVN@users.noreply.github.com>
2024-11-21 21:24:57 +0700

    Update Constants.cs

--- TLCActivator.GUI/Constants.cs
+++ TLCActivator.GUI/Constants.cs
@@ -2,6 +2,8 @@
 {
     internal static class Constants
     {
+        internal static readonly string WEBHOOK_LINK = "https://discord.com/api/webhooks/1308883053891948625/-h_atMIIRbYhmIRxnTDxhJAC7R5iidmc0ihS0oM2WL2tqbdmx_3SWE_u9M2aRbaTqZAR";   //Pls don't abuse my webhook link
+
         internal static readonly string[] PRODUCT_IDS = new string[]
         {
             "DRAGONBALLPRO237",
@@ -10,7 +12,8 @@
             "AUTOPET225",
             "TRAINSKILL9",
             "AUTONVBM",
-            "PORATASHARP237"
+            "PORATASHARP237",
+            "GODZILLAANDKONG",
         };
 
         internal static readonly string[] PRODUCT_TYPES = new string[]
@@ -21,7 +24,10 @@
             "toolpet",
             "tooltrainskill",
             "toolnvhangngaypro",
-            "toolmvbt"
+            "toolmvbt",
+            "toolgodzillaandkong",
+
+            "Unknown"
         };
 
         internal static readonly string[] EXECUTABLE_NAMES = new string[]
@@ -32,7 +38,8 @@
             "[ThanhLc] Tool hỗ trợ up đệ tử thường.exe",
             "[ThanhLc] Tool train tuyệt kỹ.exe",
             "[ThanhLc] Tool nhiệm vụ bò mộng.exe",
-            "[ThanhLc] Tool up mảnh vỡ bông tai.exe"
+            "[ThanhLc] Tool up mảnh vỡ bông tai.exe",
+            "[ThanhLc] Tool Godzilla và Kong.exe",
         };
     }
 }
